94498bdc2d094ecbb42cdb739dc6fd08759f2ba3
amitgaikwad02bright <amitgaikwad02bright@gmail.com>
2020-01-21 14:56:04 +0530

    note persist to json file sync version

--- noteManager.js
+++ noteManager.js
@@ -1,19 +1,25 @@
-let notes = [];
+
+const noteDb = require("./notesDb");
 
 function getNotes() {
-    return notes;
+    return noteDb.readNotesSync();
 }
 
 function getNoteByTitle(title) {
+    const notes = noteDb.readNotesSync();
     return notes.find(n => n.title === title);
 }
 
 function addNote(title, text) {
+   const notes = noteDb.readNotesSync();
    notes.push({title, text});
+   noteDb.saveNotesSync(notes);
 }
 
 function deleteNoteByTitle(title ) {
-    notes = notes.filter( n => n.title !== title);
+    const notes = noteDb.readNotesSync();
+    const newNotes = notes.filter( n => n.title !== title);
+    noteDb.saveNotesSync(newNotes);
  }
 
 module.exports = {
---
+++ notesDb.js
@@ -0,0 +1,47 @@
+const filePath = "notes.json";
+const fs = require("fs");
+
+module.exports = {
+    saveNotes(notes) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile("notes.json", (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve();
+            });
+        });
+    },
+    saveNotesSync(notes) {
+        try {
+           fs.writeFileSync("notes.json", JSON.stringify(notes) )
+         }
+         catch(ex) {
+             console.error("\x1b[31m merror saving notes\x1b[0m", ex );
+             throw ex;
+         }
+    },
+    readNotes() {
+        return new Promise((resolve, reject) => {
+            fs.readFile("notes.json", (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(JSON.parse(data));
+            });
+        });
+    },
+    readNotesSync() {
+        try {
+           const data = fs.readFileSync("notes.json");
+
+            if (data) {
+                return JSON.parse(data);
+            }
+        }
+        catch {
+            console.error("\x1b[31m notes.json not found\x1b[0m")
+        }
+        return [];
+    }
+}
